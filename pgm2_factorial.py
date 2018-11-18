daa98fa3427ecd81dc0e5f351581ce2391e926b5
aniruddhafrnd <aniruddhafrnd@gmail.com>
2018-11-18 11:43:14 -0700

    pgm1

--- pgm2_factorial.py
+++ pgm2_factorial.py
@@ -6,4 +6,27 @@ Suppose the following input is supplied to the program:
 Then, the output should be:
 40320
 '''
+import sys,os
 
+def cal_fact(num):
+    print ("in fuction")
+    if num == 1:
+        return num
+    fact = num * cal_fact(num-1)
+    return fact
+
+
+
+while True:
+    try:
+        in_no = int(input("Please provide integer no for factorail:-\n"))
+        type(in_no)
+        if type(in_no) == int:
+            print ("Your number is :-",in_no)
+            break
+    except ValueError:
+        print( " number is not whole number")
+
+fact_no = cal_fact(int(in_no))
+
+print ("Factorial is :-",fact_no)
